d887a36451f9314a99de06246178900eeb785a80
Ludvig Norinder <ludvignorinder@gmail.com>
2012-09-18 20:22:14 +0200

    Fixed drift in part1 + cleanup

--- part1.adb
+++ part1.adb
@@ -1,42 +1,40 @@
 with Ada.Text_IO;
-	use Ada.Text_IO;
+use Ada.Text_IO;
 
 with Ada.Calendar;
-	use Ada.Calendar;
+use Ada.Calendar;
 
 procedure part1 is
 	start : constant Time := Clock;
 	runF3 : Boolean := False;
-	loopStartTime : Time;
+	loopStartTime : Time := start;
 
 	procedure F1 is
 		now : constant Time := Clock;
 	begin 
-		Put("F1 started at: ");
+		Put("F1 started at:");
 		Put_Line(Duration'Image(now-start));
 	end F1;
 
 	procedure F2 is
 		now : constant Time := Clock;
 	begin 
-		Put("F2 started at: ");
+		Put("F2 started at:");
 		Put_Line(Duration'Image(now-start));
 	end F2;
 
 	procedure F3 is
 		now : constant Time := Clock;
 	begin 
-		Put("F3 started at: ");
+		Put("F3 started at:");
 		Put_Line(Duration'Image(now-start));
 	end F3;
 begin
 	loop
-		loopStartTime := Clock;
-
 		F1;
 		F2;
 
-		if (runF3) then
+		if runF3 then
 			delay until loopStartTime + 0.5;
 			F3;
 			runF3 := False;
@@ -44,7 +42,8 @@ begin
 			runF3 := True;
 		end if;
 
-		delay until loopStartTime + 1.0;
+		loopStartTime := loopStartTime + 1.0;
+		delay until loopStartTime;
 
 	end loop;
 end part1;
