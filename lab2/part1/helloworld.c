4a76ee48b1016603627e4bd684efc696d9f8fee0
Anders Hassis <anders.hassis@gmail.com>
2012-10-05 11:46:29 +0200

    Updated part 1

--- lab2/part1/helloworld.c
+++ lab2/part1/helloworld.c
@@ -9,10 +9,10 @@ DeclareCounter ( SysTimerCnt ) ;
 // Further, you need to define three OSEK hooks:
 
 void ecrobot_device_initialize() {
-	ecrobot_set_light_sensor_active(NXT_PORT_S1);
+	ecrobot_set_light_sensor_active(NXT_PORT_S2);
 }
 void ecrobot_device_terminate() {
-	ecrobot_set_light_sensor_inactive(NXT_PORT_S1);
+	ecrobot_set_light_sensor_inactive(NXT_PORT_S2);
 }
 
 void user_1ms_isr_type2 () { SignalCounter ( SysTimerCnt ) ; }
@@ -23,7 +23,7 @@ TASK(HelloworldTask)
 	display_goto_xy(0,0);
 	display_string("Hello World ");
 	display_update();
-	int i = ecrobot_get_light_sensor(NXT_PORT_S1);
+	int i = ecrobot_get_light_sensor(NXT_PORT_S2);
 	display_goto_xy(0,1);
 	display_int(i, 3);	
 
